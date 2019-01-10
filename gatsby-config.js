cb5c8b24714031e16718acc57159978ac52edebb
Jovi De Croock <jovi@codifly.be>
2019-01-10 16:04:39 +0100

    google

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-132184126-1`,
       },
     },
     `gatsby-plugin-feed`,
