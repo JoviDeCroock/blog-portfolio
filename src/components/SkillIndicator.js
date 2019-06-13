f4e5c4e27dcb21536a985d363d1145b693a58eb6
Jovi De Croock <decroockjovi@gmail.com>
2019-06-13 15:32:48 +0200

    feat: implement intersection observer for animations

--- src/components/SkillIndicator.js
+++ src/components/SkillIndicator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Spring } from 'react-spring'
+import { InView } from 'react-intersection-observer'
 
 const BarWrapper = styled.div`
   border: 1px solid black;
@@ -22,21 +23,33 @@ const Text = styled.p`
   margin-left: 6px;
 `
 
-const Wrapper = styled.div`
-  margin-bottom: 2px;
+const Wrapper = styled(InView)`
+  margin-bottom: 4px;
 `
 
-export default ({ title, level: finalLevel }) => (
-  <Wrapper>
-    <Text>{title}</Text>
-    <BarWrapper>
-      <Spring
-        delay={300}
-        from={{ level: 0 }}
-        to={{ level: finalLevel }}
-      >
-        {({ level}) => <Indicator level={level} />}
-      </Spring>
-    </BarWrapper>
-  </Wrapper>
-)
+const SkillIndicator = ({ title, level: finalLevel }) => {
+  const [show, setShow] = React.useState(false);
+
+  const onChange = React.useCallback(inView => {
+    if (inView) {
+      setShow(() => inView);
+    }
+  }, [])
+
+  return (
+    <Wrapper onChange={onChange}>
+      <Text>{title}</Text>
+      <BarWrapper>
+        <Spring
+          delay={300}
+          from={{ level: 0 }}
+          to={{ level: show ? finalLevel : 0 }}
+        >
+          {({ level}) => <Indicator level={level} />}
+        </Spring>
+      </BarWrapper>
+    </Wrapper>
+  )
+}
+
+export default React.memo(SkillIndicator);
--- src/pages/404.js
+++ src/pages/404.js
@@ -4,20 +4,13 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>Ah you were sniffing around to see if I had hidden any easter eggs, did you now?</p>
-      </Layout>
-    )
-  }
-}
+const NotFoundPage = ({ data: { site: { siteMetadata: { title: siteTitle } } }, location }) => (
+  <Layout location={location} title={siteTitle}>
+    <SEO title="404: Not Found" />
+    <h1>Not Found</h1>
+    <p>Ah you were sniffing around to see if I had hidden any easter eggs, did you now?</p>
+  </Layout>
+)
 
 export default NotFoundPage
 
