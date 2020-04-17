3d6cd4d7e2930d9eee71470c2bd0f772903f6745
Jovi De Croock <decroockjovi@gmail.com>
2020-04-17 02:13:15 +0200

    add hooked-head (#1)

---
+++ gatsby-ssr.js
@@ -0,0 +1,17 @@
+import { createElement } from 'react';
+import { toStatic } from 'hooked-head';
+
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  const { title, metas, lang } = toStatic();
+
+  if (lang && setHtmlAttributes) {
+    setHtmlAttributes({ lang });
+  }
+
+  setHeadComponents(
+    [
+      title && createElement('title', null, title),
+      ...metas.map((meta) => createElement('meta', meta, null)),
+    ].filter(Boolean)
+  );
+};
