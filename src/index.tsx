4e6bd5051897398b37ac68f3cb7fe52f1d03d079
Marvin Hagemeister <hello@marvinh.dev>
2022-02-13 13:40:29 +0100

    Fix misaligned indicator design

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { h, render } from 'preact'
 import { setup } from 'goober'
+import { createGlobalStyles } from 'goober/global'
 import { LocationProvider, Router } from 'preact-iso'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import posts from './pages/posts'
 import NotFound from './pages/404'
+
+const GlobalStyles = createGlobalStyles`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`
 
 setup(h)
 
@@ -12,10 +19,13 @@ export function App() {
   return (
     <LocationProvider>
       <div className="app">
+        <GlobalStyles />
         <Router>
           <Home path="/" />
           <Blog path="/blog" />
-          {posts.map(post => <post.Component path={post.path} />)}
+          {posts.map((post) => (
+            <post.Component path={post.path} />
+          ))}
           <NotFound default />
         </Router>
       </div>
