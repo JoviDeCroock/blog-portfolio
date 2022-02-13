bac3cfe1b43a34eb29bd251094614ba74d831c26
jdecroock <decroockjovi@gmail.com>
2022-02-13 15:28:18 +0100

    writings

---
+++ src/pages/posts/vdom-compilers/Strict-Equality.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'preact/hooks'
+import { RerenderTracker, DOMMutationTracker } from './common'
+
+const Counter = (props) => {
+    const [count, setCount] = useState(0)
+
+    return (
+      <div style='padding: 0px 16px'>
+        <RerenderTracker name={`Counter #${props.i}`} />
+        <p>Count: {count}</p>
+        <div style="display:flex;">
+          <button onClick={() => setCount((c) => c - 1)}>-1</button>
+          <button onClick={() => setCount((c) => c + 1)}>
+            +1
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  const Layout = (props) => {
+    const [darkMode, setDarkMode] = useState(false)
+
+    return (
+      <main style='padding: 0px 16px'>
+        <RerenderTracker name='Layout' />
+        <button onClick={() => setDarkMode((dark) => !dark)}>set {darkMode ? "Light" : "Dark"} Mode</button>
+        {props.children}
+      </main>
+    )
+  }
+
+const App = () => {
+  const [, setRerender] = useState({})
+  return (
+    <div style="border: 1px solid black; padding: 4px 8px">
+      <RerenderTracker name='App' />
+      <button onClick={() => setRerender({})}>Rerender App</button>
+      <Layout>
+        <Counter i='1' />
+        <Counter i='2' />
+      </Layout>
+    </div>
+  )
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import mdx from '@mdx-js/rollup'
+import rehypeHighlight from 'rehype-highlight'
 
 export default defineConfig({
-  plugins: [preact(), mdx()],
+  plugins: [
+    preact(),
+    mdx({
+      rehypePlugins: [rehypeHighlight]
+    })
+  ],
 });
