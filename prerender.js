3e54cfb25af626d4d0d1378feb8dcce2f464ee30
jdecroock <decroockjovi@gmail.com>
2022-07-04 15:14:40 +0200

    should work

--- prerender.js
+++ prerender.js
@@ -5,8 +5,11 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const toAbsolute = (p) => path.resolve(__dirname, p)
 
-const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
+const template = fs.readFileSync(toAbsolute('dist/index.html'), 'utf-8')
 const { render } = await import('./dist/server/entry-server.cjs')
+try {
+  fs.mkdirSync(toAbsolute('dist/blog'))
+} catch (e) {}
 
 // determine routes to pre-render from src/pages
 const routesToPrerender = fs
@@ -16,18 +19,23 @@ const routesToPrerender = fs
     return name === 'home' ? `/` : `/${name}`
   })
 
-// TODO: posts
+const postsToPrerender = [
+  '/blog/vdom-compilers',
+  '/blog/controlled-inputs',
+  '/blog/suspense-data-ssr',
+  '/blog/state-in-vdom',
+]
 
 ;(async () => {
   // pre-render each route...
-  for (const url of routesToPrerender) {
-    const context = {}
-    const result = await render(url, context)
+  for (const url of [...routesToPrerender, ...postsToPrerender]) {
+    const result = await render(url)
 
     let html = template.replace(`<div id="main"></div>`, `<div id="main">${result.body}</div>`)
+    console.log(result.css)
     html = html.replace(`<!-- STYLE CONTENT -->`, `<style id="_goober">${result.css}</style>`)
 
-    const filePath = `dist/static${url === '/' ? '/index' : url}.html`
+    const filePath = `dist${url === '/' ? '/index' : url}.html`
     fs.writeFileSync(toAbsolute(filePath), html)
     console.log('pre-rendered:', filePath)
   }
