4db8f35af4e362096fffaea6ff09fe2f206b0355
Jovi De Croock <decroockjovi@gmail.com>
2022-07-04 21:04:10 +0200

    async routes (#18)

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,4 +1,5 @@
 import renderToString from 'preact-render-to-string'
+import prepass from 'preact-ssr-prepass'
 import { LocationProvider } from 'preact-iso'
 import { extractCss } from 'goober'
 import { toStatic } from 'hoofd/preact'
@@ -9,13 +10,15 @@ global.location = {
   origin: 'http://localhost:3000'
 }
 
-export function render(url) {
-  const html = renderToString(
+export async function render(url) {
+  let jsx = (
     // @ts-ignore
     <LocationProvider url={url}>
       <App />
     </LocationProvider>
   )
+  await prepass(jsx)
+  const html = renderToString(jsx)
   return {
     body: html,
     meta: toStatic(),
