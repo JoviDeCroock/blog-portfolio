5e7ded21689cfd0e780e7a9069044f1490f44b29
jdecroock <decroockjovi@gmail.com>
2022-08-28 14:43:47 +0200

    upgrade vite

--- prerender.js
+++ prerender.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const toAbsolute = (p) => path.resolve(__dirname, p)
 
 const template = fs.readFileSync(toAbsolute('dist/index.html'), 'utf-8')
-const { render } = await import('./dist/server/entry-server.cjs')
+const { render } = await import('./dist/server/entry-server.js')
 try {
   fs.mkdirSync(toAbsolute('dist/blog'))
 } catch (e) {}
