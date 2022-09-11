7e7223a634ddb2dcf0da0797d4877129fa8a838b
jdecroock <decroockjovi@gmail.com>
2022-09-11 12:44:32 +0200

    typo

--- src/pages/posts/preact-use-id/documentProps.ts
+++ src/pages/posts/preact-use-id/documentProps.ts
@@ -2,7 +2,7 @@ export const documentProps = {
   title: 'Unique identifier in Preact',
   image: 'https://www.jovidecroock.com/preact-use-id.jpg',
   description:
-    'Getting unique tree-identifiers consistent across server and cclient.',
+    'Getting unique tree-identifiers consistent across server and client.',
   path: '/blog/preact-use-id',
   tags: ['vdom', 'front-end', 'open-source']
 }
