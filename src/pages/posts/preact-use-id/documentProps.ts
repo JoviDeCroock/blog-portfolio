1176a549fef1222148c30c2686e70efe5c2ed7d8
jdecroock <decroockjovi@gmail.com>
2022-09-11 12:44:23 +0200

    meta

--- src/pages/posts/preact-use-id/documentProps.ts
+++ src/pages/posts/preact-use-id/documentProps.ts
@@ -1,6 +1,6 @@
 export const documentProps = {
   title: 'Unique identifier in Preact',
-  image: 'https://www.jovidecroock.com/preact-use-id.png',
+  image: 'https://www.jovidecroock.com/preact-use-id.jpg',
   description:
     'Getting unique tree-identifiers consistent across server and cclient.',
   path: '/blog/preact-use-id',
