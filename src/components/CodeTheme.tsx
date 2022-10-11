0735412084faf49c8aa7da9c359c5013f44bdb56
jdecroock <decroockjovi@gmail.com>
2022-10-11 13:18:25 +0200

    optim

--- src/components/CodeTheme.tsx
+++ src/components/CodeTheme.tsx
@@ -4,6 +4,12 @@ const CodeTheme = () => {
   useLink({
     rel: 'preconnect',
     href: 'https://cdnjs.cloudflare.com/',
+    crossorigin: 'anonymous'
+  })
+
+  useLink({
+    rel: 'preload',
+    href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css',
   })
 
   useLink({
