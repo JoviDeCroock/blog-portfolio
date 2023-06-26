9c5b52dfc061a630cd2649181535f2c09c56b5c5
jdecroock <decroockjovi@gmail.com>
2023-06-26 18:51:52 +0200

    start writing

---
+++ src/pages/posts/graphql-dx/documentProps.ts
@@ -0,0 +1,7 @@
+export const documentProps = {
+  title: 'GraphQL Developer Experience',
+  description: '',
+  image: 'https://www.jovidecroock.com/graphql-dx.png',
+  path: '/blog/graphql-dx',
+  tags: ['developer experience', 'graphql', 'front-end']
+}
