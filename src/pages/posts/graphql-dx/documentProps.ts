acea769d5f01f6a7d2371f1aec10dbc860a99730
jdecroock <decroockjovi@gmail.com>
2023-07-22 08:54:29 +0200

    write about persisted operations

--- src/pages/posts/graphql-dx/documentProps.ts
+++
@@ -1,7 +0,0 @@
-export const documentProps = {
-  title: 'GraphQL Developer Experience',
-  description: '',
-  image: 'https://www.jovidecroock.com/graphql-dx.png',
-  path: '/blog/graphql-dx',
-  tags: ['developer experience', 'graphql', 'front-end']
-}
---
+++ src/pages/posts/persisted-operations/documentProps.ts
@@ -0,0 +1,7 @@
+export const documentProps = {
+  title: 'GraphQL Persisted Operations',
+  description: '',
+  image: 'https://www.jovidecroock.com/persisted-operations.png',
+  path: '/blog/persisted-operations',
+  tags: ['security', 'graphql', 'front-end']
+}
