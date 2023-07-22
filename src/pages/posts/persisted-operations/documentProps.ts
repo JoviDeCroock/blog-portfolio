4a89c1d61da3839f5981b8383c8edca63b122091
jdecroock <decroockjovi@gmail.com>
2023-07-22 11:09:31 +0200

    finishing touches

--- src/pages/posts/persisted-operations/documentProps.ts
+++ src/pages/posts/persisted-operations/documentProps.ts
@@ -1,7 +1,7 @@
 export const documentProps = {
   title: 'GraphQL Persisted Operations',
-  description: '',
+  description: 'Persisted operations are a way to introduce caching and additional security to your GraphQL Endpoint.',
   image: 'https://www.jovidecroock.com/persisted-operations.png',
   path: '/blog/persisted-operations',
-  tags: ['security', 'graphql', 'front-end']
+  tags: ['security', 'performance', 'graphql', 'front-end']
 }
