1ce52e6d0a82b559c12aa555b4fc56cf2282abac
Jovi De Croock <decroockjovi@gmail.com>
2023-12-28 10:34:30 +0100

    graphql dev workflow

---
+++ src/pages/posts/graphql-development-workflow/documentProps.ts
@@ -0,0 +1,8 @@
+export const documentProps = {
+  title: 'GraphQL Development Workflow',
+  description:
+    'An opinionated development workflow I have enjoyed using when it comes to GraphQL.',
+  image: 'https://www.jovidecroock.com/graphql-development-workflow.png',
+  path: '/blog/graphql-development-workflow',
+  tags: ['graphql', 'front-end'],
+}
