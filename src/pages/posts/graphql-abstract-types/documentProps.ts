39389a204c6aef2b493735d412372662473772a6
Jovi De Croock <decroockjovi@gmail.com>
2024-01-16 08:36:27 +0100

    introduction

---
+++ src/pages/posts/graphql-abstract-types/documentProps.ts
@@ -0,0 +1,8 @@
+export const documentProps = {
+  title: 'GraphQL Caching Abstract Types',
+  description:
+    '',
+  image: 'https://www.jovidecroock.com/graphql-abstract-types.png',
+  path: '/blog/graphql-abstract-types',
+  tags: ['graphql', 'front-end'],
+}
