a9238110f3cb7449b2682c1d4128d568b235d0e4
Jovi De Croock <decroockjovi@gmail.com>
2024-01-26 16:13:56 +0100

    info

--- src/pages/posts/graphql-abstract-types/documentProps.ts
+++ src/pages/posts/graphql-abstract-types/documentProps.ts
@@ -1,7 +1,7 @@
 export const documentProps = {
   title: 'GraphQL Caching Abstract Types',
   description:
-    '',
+    'A quick exploration of how a client-side cache looks at abstract and concrete types in GraphQL.',
   image: 'https://www.jovidecroock.com/graphql-abstract-types.png',
   path: '/blog/graphql-abstract-types',
   tags: ['graphql', 'front-end'],
