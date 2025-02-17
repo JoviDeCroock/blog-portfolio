209d4eba3a8959cedc229908892460a684ae5a90
jdecroock <decroockjovi@gmail.com>
2025-02-17 16:43:48 +0100

    SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,9 +5,11 @@ type SEOProps = {
   description: string
   keywords?: string[]
   image?: string
+  url?: string
 }
 
 const author = 'Jovi De Croock'
+const baseUrl = 'https://jovidecroock.com'
 
 const SEO = (props: SEOProps) => {
   useLang('en')
@@ -21,17 +23,28 @@ const SEO = (props: SEOProps) => {
     name: 'keywords',
     content: props.keywords ? props.keywords.join(', ') : '',
   })
+  // Canonical URL
+  useMeta({
+    property: 'canonical',
+    content: props.url ? `${baseUrl}${props.url}` : baseUrl,
+  })
+  // Robots
+  useMeta({
+    name: 'robots',
+    content: 'index,follow'
+  })
 
-  // Twitta
+  // Twitter
   useMeta({
     name: 'twitter:card',
     content: props.image ? 'summary_large_image' : 'summary',
   })
-  useMeta({ name: 'twitter:creator', content: 'jovidec' })
+  useMeta({ name: 'twitter:creator', content: '@jovidec' })
+  useMeta({ name: 'twitter:site', content: '@jovidec' })
   useMeta({ name: 'twitter:title', content: props.title })
   useMeta({
     name: 'twitter:image',
-    content: props.image || 'https://jovidecroock.com/me.jpg',
+    content: props.image || `${baseUrl}/me.jpg`,
   })
   useMeta({
     name: 'twitter:description',
@@ -42,12 +55,20 @@ const SEO = (props: SEOProps) => {
   useMeta({ property: 'og:title', content: props.title })
   useMeta({
     property: 'og:image',
-    content: props.image || 'https://jovidecroock.com/me.jpg',
+    content: props.image || `${baseUrl}/me.jpg`,
   })
   useMeta({ property: 'og:type', content: 'website' })
   useMeta({
     property: 'og:description',
     content: props.description,
+  })
+  useMeta({
+    property: 'og:url',
+    content: props.url ? `${baseUrl}${props.url}` : baseUrl,
+  })
+  useMeta({
+    property: 'og:site_name',
+    content: 'Jovi De Croock',
   })
 
   return null
