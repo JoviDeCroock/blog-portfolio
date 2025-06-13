83ba255844548b82a228620862e3daa559e562f9
Jovi De Croock <decroockjovi@gmail.com>
2025-06-13 19:42:50 +0200

    Wrap in main

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,14 @@
 import SEO from '../components/Seo'
 
 const NotFoundPage = () => (
-  <>
+  <main>
     <SEO title="Not found" description="Not found" />
     <h1>Not Found</h1>
     <p>
       Ah you were sniffing around to see if I had hidden any easter eggs, did
       you now?
     </p>
-  </>
+  </main>
 )
 
 export default NotFoundPage
