b4cb8bedaae1c7cfca9d78d5462e239ef92d5d80
jdecroock <decroockjovi@gmail.com>
2025-06-13 16:20:56 +0200

    Fixes

--- src/utils/generateRss.ts
+++ src/utils/generateRss.ts
@@ -1,4 +1,3 @@
-import { XMLBuilder } from 'fast-xml-parser';
 import { posts, type Post } from '../pages/Blog';
 
 // Extend the Post interface to include createdAt for RSS purposes
@@ -21,58 +20,38 @@ export function generateRSSFeed(): string {
     .filter(post => !post.external && (post as any).createdAt) // Only include posts with createdAt
     .map(post => post as RSSPost)
     .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-  const rssData = {
-    "?xml": {
-      "@_version": "1.0",
-      "@_encoding": "UTF-8"
-    },
-    rss: {
-      "@_version": "2.0",
-      "@_xmlns:atom": "http://www.w3.org/2005/Atom",
-      channel: {
-        title: siteConfig.title,
-        description: siteConfig.description,
-        link: siteConfig.siteUrl,
-        language: siteConfig.language,
-        managingEditor: siteConfig.managingEditor,
-        webMaster: siteConfig.webMaster,
-        lastBuildDate: new Date().toUTCString(),
-        generator: "Custom RSS Generator",
-        "atom:link": {
-          "@_href": `${siteConfig.siteUrl}/rss.xml`,
-          "@_rel": "self",
-          "@_type": "application/rss+xml"
-        },
-        item: blogPosts.map(post => ({
-          title: post.title,
-          description: post.description,
-          link: `${siteConfig.siteUrl}${post.path}`,
-          guid: {
-            "#text": `${siteConfig.siteUrl}${post.path}`,
-            "@_isPermaLink": "true"
-          },
-          pubDate: new Date(post.createdAt).toUTCString(),
-          category: post.tags,
-          ...(post.image && {
-            enclosure: {
-              "@_url": post.image,
-              "@_type": "image/jpeg",
-              "@_length": "0"
-            }
-          })
-        }))
-      }
-    }
-  };
 
-  const builder = new XMLBuilder({
-    attributeNamePrefix: "@_",
-    textNodeName: "#text",
-    ignoreAttributes: false,
-    format: true,
-    indentBy: "  ",
-    suppressEmptyNode: true
-  });
+  // Build RSS manually to avoid XML attribute issues
+  const rssItems = blogPosts.map(post => {
+    const enclosure = post.image ? 
+      `    <enclosure url="${post.image}" type="image/jpeg" length="0" />` : '';
+    
+    const categories = post.tags.map(tag => `    <category>${tag}</category>`).join('\n');
+    
+    return `  <item>
+    <title><![CDATA[${post.title}]]></title>
+    <description><![CDATA[${post.description}]]></description>
+    <link>${siteConfig.siteUrl}${post.path}</link>
+    <guid isPermaLink="true">${siteConfig.siteUrl}${post.path}</guid>
+    <pubDate>${new Date(post.createdAt).toUTCString()}</pubDate>
+${categories}
+${enclosure}
+  </item>`;
+  }).join('\n');
 
-  return builder.build(rssData);
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title><![CDATA[${siteConfig.title}]]></title>
+    <description><![CDATA[${siteConfig.description}]]></description>
+    <link>${siteConfig.siteUrl}</link>
+    <language>${siteConfig.language}</language>
+    <managingEditor>${siteConfig.managingEditor}</managingEditor>
+    <webMaster>${siteConfig.webMaster}</webMaster>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    <generator>Custom RSS Generator</generator>
+    <atom:link href="${siteConfig.siteUrl}/rss.xml" rel="self" type="application/rss+xml" />
+${rssItems}
+  </channel>
+</rss>`;
 }
