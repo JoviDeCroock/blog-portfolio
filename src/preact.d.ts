107d2a8d4bb30761b803ca91535fbe5db79bd5cb
Jovi De Croock <decroockjovi@gmail.com>
2025-06-27 20:55:58 +0200

    Fixes

--- src/preact.d.ts
+++ src/preact.d.ts
@@ -1,5 +1,13 @@
 // preact.d.ts
-import JSX = preact.JSX;
+/// <reference types="preact" />
+
+import { JSX } from 'preact';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends preact.JSX.IntrinsicElements {}
+  }
+}
 
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element;
