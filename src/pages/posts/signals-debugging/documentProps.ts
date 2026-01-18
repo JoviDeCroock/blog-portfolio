2b0ec2b6369b5027d61a41a8f5049573565d113f
Jovi De Croock <decroockjovi@gmail.com>
2026-01-18 11:00:56 +0100

    Initial post

---
+++ src/pages/posts/signals-debugging/documentProps.ts
@@ -0,0 +1,9 @@
+export const documentProps = {
+  createdAt: '2025-12-09',
+  title: 'The Browser Has Everything You Need',
+  description:
+    'Stop treating SPAs and SSR as opposing paradigms. The browser already loads resources in parallel but you have to use it.',
+  image: 'https://www.jovidecroock.com/platform.jpg',
+  path: '/blog/platform',
+  tags: ['front-end', 'performance', 'web'],
+}
