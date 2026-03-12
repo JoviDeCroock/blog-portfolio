e75b440994075cd6d786094c7ffb2d4ec60503d5
Jovi De Croock <decroockjovi@gmail.com>
2026-03-12 08:40:34 +0100

    Nailed it?

--- src/pages/posts/graphql-rest-trpc-llms/documentProps.ts
+++ src/pages/posts/graphql-rest-trpc-llms/documentProps.ts
@@ -1,8 +1,8 @@
 export const documentProps = {
   createdAt: '2026-03-11',
-  title: 'I changed my mind about GraphQL, REST and tRPC',
+  title: 'I have changed my mind about GraphQL',
   description:
-    'LLM-driven development changed how I think about the tradeoffs between GraphQL, REST and tRPC.',
+    'Boilerplate became cheap, but GraphQL still wins when a durable schema contract matters across teams and clients.',
   image: 'https://www.jovidecroock.com/graphql-rest-trpc-llms.jpg',
   path: '/blog/graphql-rest-trpc-llms',
   tags: ['graphql', 'rest', 'trpc', 'llm', 'front-end'],
