c71a79585bfc41f95aea7a2538e6f3dd05b352e1
jdecroock <decroockjovi@gmail.com>
2026-07-02 09:21:42 +0200

    Fed up with Belgium

---
+++ src/pages/posts/fed-up-with-belgium/documentProps.ts
@@ -0,0 +1,9 @@
+export const documentProps = {
+  createdAt: '2026-07-02',
+  title: 'Fed up with Belgium',
+  description:
+    'A country drowning in debt decides the problem is the people still willing to build in it. A personal account of paying gladly for almost a decade, and the moment that changed.',
+  image: 'https://www.jovidecroock.com/fed-up-with-belgium.jpg',
+  path: '/blog/fed-up-with-belgium',
+  tags: ['thinking', 'personal'],
+}
